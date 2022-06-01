7b62eb8d0ecc390ca45525e71159fe9028bb916b
Kamila <kamilaaadamska@gmail.com>
2022-06-01 19:26:30 +0200

    Dopisuje rzeczy  w main.c

--- main.c
+++ main.c
@@ -1,29 +1,29 @@
-@@ - 1, 7 + 1, 60 @@
 #include <stdio.h>
 #include <pthread.h>
 
-int main
+int main;
 int a_chilluje, a_czeka, b_czeka, b_chilluje;
 
 void* Auto(void* numer) {
 
     int num = (int*)numer
         Bool strona = 0; //0-A 1-B
+    
     while (1) {
         //faza nic nie robienia
         sleep(rand() % 20 + 5)
-            //faza nic nie robienia - koniec
+        //faza nic nie robienia - koniec
 
 
 
-            //faza czekania
-            lock(mutex_dane);
+        //faza czekania
+        lock(mutex_dane);
         if (strona == 0) {
-            a_chilluje–;
+            a_chilluje;
             a_czeka++;
         }
         else {
-            b_chilluje–;
+            b_chilluje;
             b_czeka++;
         }
         unlock(mutex_dane);
@@ -32,35 +32,46 @@ void* Auto(void* numer) {
         sleep(rand() % 4 + 1);
         if (strona == 0) {
             lock(mutex_dane);
-            printf(“A - % d % d >> > [>> % d >> ] << < % d % d - B ”, a_chilluje, a_czeka, *num, b_czeka, b_chilluje);
+            printf(A - % d % d >> > [>> % d >> ] << < % d % d - B , a_chilluje, a_czeka, *num, b_czeka, b_chilluje);
             unlock(mutex_dane);
         }
         else {
             lock(mutex_dane);
-            printf(“A - % d % d >> > [<< % d << ] << < % d % d - B ”, a_chilluje, a_czeka, *num, b_czeka, b_chilluje);
+            printf(A - % d % d >> > [<< % d << ] << < % d % d - B , a_chilluje, a_czeka, *num, b_czeka, b_chilluje);
             unlock(mutex_dane);
         }
-        strona ^= 1; //zmienia stronê
+        strona ^= 1; //zmienia stron
 
         lock(mutex_dane);
         if (strona == 0) {
-            b_czeka–;
+            b_czeka;
             a_chilluj++;
         }
         else {
-            a_czeka–;
+            a_czeka;
             b_chilluj++;
         }
         unlock(mutex_dane);
         unlock(mutex_most);
     }
 
+}
+
     int main(int argc, char** argv)
     {
 
         //pobieranie liczby aut
-        car_number = atoi(argv[1]);
-        
+        int car_number = atoi(argv[1]);
+        //wszystkie auta czekajÄ… ze strony A
+        a_chilluje = car_number; 
+
+        pthread_t cars[car_number];
+
+        for(i = 0; i<car_number; i++){
+            int result = pthread_create(&cars[i], NULL, Auto, &i);
+            if (result != 0) { perror("Could not create thread."); }
+        }
+
         
         printf("Hello");
         return 0;
