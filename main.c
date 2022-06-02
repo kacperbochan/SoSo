ad4435eb9173b477a41777697157fe2ed8bab30d
Bartłomiej Dec <bapidec@gmail.com>
2022-06-02 16:52:14 +0200

    bapbapbap

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-int main;
 int a_chilluje, a_czeka, b_czeka, b_chilluje;
 
 void* Auto(void* numer) {
-
-    int num = (int*)numer
-        Bool strona = 0; //0-A 1-B
+    int* num = (int*)numer;
+    Bool strona = 0; //0-A 1-B
     
     while (1) {
         //faza nic nie robienia
         sleep(rand() % 20 + 5)
         //faza nic nie robienia - koniec
-
-
 
         //faza czekania
         lock(mutex_dane);
@@ -57,22 +54,27 @@ void* Auto(void* numer) {
 
 }
 
-    int main(int argc, char** argv)
-    {
+int main(int argc, char** argv)
+{
 
-        //pobieranie liczby aut
-        int car_number = atoi(argv[1]);
-        //wszystkie auta czekają ze strony A
-        a_chilluje = car_number; 
+    //pobieranie liczby aut
+    int car_number = atoi(argv[1]);
+    int* liczby = (int*)malloc(sizeof(int)*car_number);
+    //wszystkie auta czekają ze strony A
+    a_chilluje = car_number; 
 
-        pthread_t cars[car_number];
+    pthread_t cars[car_number];
 
-        for(i = 0; i<car_number; i++){
-            int result = pthread_create(&cars[i], NULL, Auto, &i);
-            if (result != 0) { perror("Could not create thread."); }
-        }
-
-        
-        printf("Hello");
-        return 0;
+    for(int i = 0; i<car_number; i++){
+        liczby[i] = i;
+        int result = pthread_create(&cars[i], NULL, Auto, &liczby[i]);
+        if (result != 0) { perror("Could not create thread."); }
     }
+    for(int i = 0; i<car_number; i++){
+        int result = pthread_join(cars[i], NULL);
+        if (result != 0) { perror("Could not create thread."); }
+    }
+    
+    printf("Hello\n");
+    return 0;
+}
