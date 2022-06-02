bbdb8394ea3b2e503e5afa4e7363bf5d7121cad4
kacperbochan <45052903+kacperbochan@users.noreply.github.com>
2022-06-02 17:32:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,60 +1,79 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <pthread.h>
+
+pthread_mutex_t mutex_most, mutex_dane;
 
 int a_chilluje, a_czeka, b_czeka, b_chilluje;  
 
 void * Auto(void *numer){
 
-	int num = (int *) numer
-	Bool strona = 0; //0-A 1-B
+	int* num = (int *) numer;
+
+    printf("dolacza -%d \n ", *num);
+	char strona = 0; //0-A 1-B
 	while(1){
 		//faza nic nie robienia
-    sleep(rand()%20+5)
+    sleep(rand()%20+5);
         //faza nic nie robienia - koniec
 
 
 
         //faza czekania
-    lock(mutex_dane);
+    pthread_mutex_lock(&mutex_dane);
         if(strona == 0){
-			a_chilluje–;
+			a_chilluje--;
 			a_czeka++;
 		}
 		else{
-			b_chilluje–;
+			b_chilluje--;
 			b_czeka++;
 		}
-    unlock(mutex_dane);
+    pthread_mutex_unlock(&mutex_dane);
 
-    lock(mutex_most);
+    pthread_mutex_lock(&mutex_most);
 	    sleep(rand()%4+1);
 	    if(strona==0){
-            lock(mutex_dane);
-                printf(“A-%d %d>>> [>> %d >>] <<<%d %d-B ”, a_chilluje, a_czeka, *num ,b_czeka, b_chilluje);
-            unlock(mutex_dane);
+            pthread_mutex_lock(&mutex_dane);
+                printf("A-%d %d>>> [>> %d >>] <<<%d %d-B ", a_chilluje, a_czeka, *num ,b_czeka, b_chilluje);
+            pthread_mutex_unlock(&mutex_dane);
         }
         else{
-            lock(mutex_dane);
-                printf(“A-%d %d>>> [<< %d <<] <<<%d %d-B ”, a_chilluje, a_czeka, *num ,b_czeka, b_chilluje);
-            unlock(mutex_dane);
+            pthread_mutex_lock(&mutex_dane);
+                printf("A-%d %d>>> [<< %d <<] <<<%d %d-B ", a_chilluje, a_czeka, *num ,b_czeka, b_chilluje);
+            pthread_mutex_unlock(&mutex_dane);
         }
         strona ^= 1; //zmienia stronę
 
-        lock(mutex_dane);
+        pthread_mutex_lock(&mutex_dane);
             if(strona == 0){
-                b_czeka–;
-                a_chilluj++;
+                b_czeka--;
+                a_chilluje++;
             }
             else{
-                a_czeka–;
-                b_chilluj++;
+                a_czeka--;
+                b_chilluje++;
             }
-        unlock(mutex_dane);
-    unlock(mutex_most);
+        pthread_mutex_unlock(&mutex_dane);
+    pthread_mutex_unlock(&mutex_most);
+    }
 }
 
 int main(int argc, char** argv)
 {
+
+    if (pthread_mutex_init(&mutex_most, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
+    if (pthread_mutex_init(&mutex_dane, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
+
     //pobieranie liczby aut
     int car_number = atoi(argv[1]);
     int* liczby = (int*)malloc(sizeof(int)*car_number);
